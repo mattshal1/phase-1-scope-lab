639f5572721cb2145b0e9d37517bc766cbddd27f
Ahmed Shalash <ah.shalash@gmail.com>
2024-02-05 23:43:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution in this file!
+var customerName;
+
+function upperCaseCustomerName() {
+    if (customerName) {
+        return customerName.toUpperCase();
+    } else {
+        return "No customer name provided.";
+    }
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'still not bob';
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'someone else'; 
+}
+
+// Testing the functions
+customerName = 'Alice';
+console.log(upperCaseCustomerName()); 
+
+setBestCustomer();
+console.log(bestCustomer); 
+
+overwriteBestCustomer();
+console.log(bestCustomer); 
+
+console.log(leastFavoriteCustomer); 
+
